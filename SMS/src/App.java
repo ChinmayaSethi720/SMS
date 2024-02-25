fc9d8f0a2ad0d67404ad39674325349bf6d35108
Chinmaya Priyadarshan <chinmayasethi720@gmail.com>
2024-02-25 17:02:12 +0530

    Added Skeleton

--- SMS/src/App.java
+++ SMS/src/App.java
@@ -1,7 +1,28 @@
+import java.util.*;
 public class App {
-
+		
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc= new Scanner(System.in);
+		while(true) {
+			System.out.println("1. Show\n2. Add\n3. Delete\n4. Exit");
+			int n= sc.nextInt();
+			
+			switch(n) {
+				case(1):
+					System.out.println("Display All values");
+					break;
+				case(2):
+					System.out.println("Add Details");
+					break;
+				case(3):
+					System.out.println("Delete values");
+					break;
+				case(4):
+					System.out.println("Exit Successful");
+					return;
+			}
+		}
+		
 
 	}
 
