05f384beca43f801ade7856266bf029678db69aa
Chinmaya Priyadarshan <chinmayasethi720@gmail.com>
2024-02-27 19:23:22 +0530

    Created 'credential' table in the database. Initialized the code with Login and Signup.

--- SMS/src/App.java
+++ SMS/src/App.java
@@ -10,23 +10,34 @@ public class App {
 		System.out.println(con);
 		while(true) {
 			
-			System.out.println("1. Show\n2. Add\n3. Delete\n4. Exit");
+			System.out.println("1. Log In\n2. Sign Up");
 			int n= sc.nextInt();
-
-			switch(n) {
-				case(1):
-					System.out.println("Display All values");
+			
+			switch (n) {
+				case 1:
+					System.out.println("Log In");
 					break;
-				case(2):
-					System.out.println("Add Details");
+				case 2:
+					System.out.println("Sign Up");
 					break;
-				case(3):
-					System.out.println("Delete values");
-					break;
-				case(4):
-					System.out.println("Exit Successful");
-					return;
 			}
+				
+
+
+//			switch(n) {
+//				case(1):
+//					System.out.println("Display All values");
+//					break;
+//				case(2):
+//					System.out.println("Add Details");
+//					break;
+//				case(3):
+//					System.out.println("Delete values");
+//					break;
+//				case(4):
+//					System.out.println("Exit Successful");
+//					return;
+//			}
 		}
 		
 
